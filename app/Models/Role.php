abcba808c6d4cf9d147af1751ae271a5c09c9862
code-arif <ahsanullahariful6460@gmail.com>
2026-03-21 20:58:55 +0600

    prepare role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
-    //
+    protected $fillable = ['name', 'display_name', 'description'];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'role_user');
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission');
+    }
+
+    public function hasPermission(string $permission): bool
+    {
+        return $this->permissions->contains('name', $permission);
+    }
 }
