67c08f0017b2aae16b2968b745a1e39bff013163
code-arif <ahsanullahariful6460@gmail.com>
2026-03-21 20:59:46 +0600

    prepare permission model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Permission extends Model
 {
-    //
+    protected $fillable = ['name', 'display_name', 'group'];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'role_permission');
+    }
 }
