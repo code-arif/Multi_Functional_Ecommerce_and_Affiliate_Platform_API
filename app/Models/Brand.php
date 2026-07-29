151714685cc3ed929cb371bf35c4da0c4e322885
code-arif <ahsanullahariful6460@gmail.com>
2026-07-29 20:47:13 +0600

    feat: initialize core domain models including Brand, Category, CmsPage, Product, and AffiliateProduct

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\HasSlug;
+use Modules\Catalog\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
