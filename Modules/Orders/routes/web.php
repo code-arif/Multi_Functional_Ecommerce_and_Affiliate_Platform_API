476b15d9ccb0123624f8674adea2c5e32b3e4293
code-arif <ahsanullahariful6460@gmail.com>
2026-08-05 14:50:52 +0600

    feat: initialize api and web route files for Orders module

--- Modules/Orders/routes/web.php
+++ Modules/Orders/routes/web.php
@@ -2,7 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Orders\Http\Controllers\OrdersController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('orders', OrdersController::class)->names('orders');
-});
